ac9812fc5e4aed273e21454d595f9fe1a5e17ae7
MrGForce <willortizloza@hotmail.com>
2021-09-12 01:48:21 +1000

    Start the project to connect to the database

--- SQLDataProvider/SQLServerDataAccess.cs
+++ SQLDataProvider/SQLServerDataAccess.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SQLDataProvider
 {
-    public class SQLServerDataAccess
+    /// <summary>
+    /// This class establish communication with the DataBase.
+    /// </summary>
+    internal static class SQLServerDataAccess
     {
+        static ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["OrdersMgmtConnectionString"];
+        static SqlConnection conn = new SqlConnection(settings.ConnectionString);
+        static SqlCommand cmnd = new SqlCommand();
+
+        public static SqlCommand GetSqlCommand(string sql)
+        {
+            cmnd.CommandText = sql;
+            cmnd.Connection = conn;
+            return cmnd;
+        }
+        public static void OpenConnection()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                    Console.WriteLine("The connection is " + conn.State.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Open connection failed: " + ex.Message);
+            }
+        }
+        public static void CloseConnection()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                    Console.WriteLine("The connection is " + conn.State.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Close connection error: " + ex.Message);
+            }
+
+        }
     }
 }
---
+++ SQLDataProvider/SqlDataProvider.cs
@@ -0,0 +1,105 @@
+﻿using DataModels;
+using DataProvider;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLDataProvider
+{
+    /// <summary>
+    /// Defines a class to perform CRUD operations on the database and expose the
+    /// required data for the application.
+    /// </summary>
+    public class SqlDataProvider : IOrdersDataProvider
+    {
+       
+        public List<Order> Orders => throw new NotImplementedException();
+
+        public List<StockItem> StockItems => throw new NotImplementedException();
+
+        public void AddNewOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrderItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrderById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetStatusString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StockItem GetStockItembyId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StockItem> GetStockItems()
+        {
+            List<StockItem> stockItems = new List<StockItem>();
+
+            SQLServerDataAccess.OpenConnection();
+            SqlCommand command = SQLServerDataAccess.GetSqlCommand("[sp_SelectStockItems]");
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                StockItem item = new StockItem()
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Price = reader.GetDecimal(2),
+                    InStock = reader.GetInt32(3)
+                };
+                stockItems.Add(item);
+            }
+            SQLServerDataAccess.CloseConnection();
+            return stockItems;
+        }
+
+        public void InsertOrderItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStockItemAmount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
