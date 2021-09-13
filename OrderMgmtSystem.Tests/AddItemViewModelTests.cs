c5f2585d812c0e447e5f8cc11aa38ad1aa73fa29
MrGForce <willortizloza@hotmail.com>
2021-09-14 01:34:57 +1000

    Add unit test project to solution

---
+++ OrderMgmtSystem.Tests/AddItemViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿using DataModels;
+using DataProvider;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderMgmtSystem.Services;
+using OrderMgmtSystem.Services.Dialogs.TestDialogs;
+using OrderMgmtSystem.ViewModels;
+using System.Linq;
+
+namespace OrderMgmtSystem.Tests
+{
+    [TestClass]
+    public class AddItemViewModelTests
+    {
+        private static readonly IOrdersDataProvider ordersData = new RandomDataProvider();
+        private static readonly IDialogService mockDialogService = new MockDialogService();
+        private static readonly DialogViewModelBase<int> mockDialogVM = new MockDialogViewModel();
+        private readonly AddItemViewModel viewModel = new AddItemViewModel(ordersData.StockItems, mockDialogService, mockDialogVM);
+
+        [TestMethod]
+        public void OnAddItem_StockItemIsUpdated()
+        {
+            // Arrange
+            StockItem selectedItem = ordersData.StockItems.FirstOrDefault();
+            int availableStock = selectedItem.InStock;
+            // Act
+            viewModel.AddItem(selectedItem);
+            // Assert
+            Assert.IsTrue(selectedItem.InStock == availableStock - 3 || selectedItem.InStock == 0);
+        }
+
+    }
+}
