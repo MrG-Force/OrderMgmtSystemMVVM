100b4e2a7467ad238aba7c9466cae000536a4363
MrGForce <willortizloza@hotmail.com>
2021-09-13 22:42:58 +1000

    Add ComposeObjects function to App.xaml.cs(Bootsrapper)

--- OrderMgmtSystem/App.xaml.cs
+++ OrderMgmtSystem/App.xaml.cs
@@ -9,18 +9,23 @@ namespace OrderMgmtSystem
     /// </summary>
     public partial class App : Application
     {
-        private IOrdersDataProvider _Data;
+        //private IOrdersDataProvider _Data;
         public static Window CurrentMainWindow => Current.MainWindow; // to easily assign the owner of child windows and popups
         protected override void OnStartup(StartupEventArgs e)
         {
-            _Data = new RandomDataProvider();
-            MainWindow = new MainWindow()
-            {
-                DataContext = new MainWindowViewModel(_Data)
-            };
+            ComposeObjects();
             MainWindow.Show();
 
             base.OnStartup(e);
+        }
+        private static void ComposeObjects()
+        {
+            var ordersData = new RandomDataProvider();
+            var viewModel = new MainWindowViewModel(ordersData);
+            Application.Current.MainWindow = new MainWindow
+            {
+                DataContext = viewModel
+            };
         }
     }
 }
