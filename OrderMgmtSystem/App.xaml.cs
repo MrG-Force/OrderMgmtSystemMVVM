db2ed80fda3e3122deda4bb1eecc86e257e9602b
MrGForce <willortizloza@hotmail.com>
2021-09-14 14:38:08 +1000

    Add dependency injection, MainWindow is not responsible for creating VModels

--- OrderMgmtSystem/App.xaml.cs
+++ OrderMgmtSystem/App.xaml.cs
@@ -18,10 +18,25 @@ namespace OrderMgmtSystem
 
             base.OnStartup(e);
         }
+
+        /// <summary>
+        /// A bootstrapper method respponsible for the instantiation of the view models 
+        /// for the application.
+        /// </summary>
         private static void ComposeObjects()
         {
             var ordersData = new RandomDataProvider();
-            var viewModel = new MainWindowViewModel(ordersData);
+            var currentViewModel = new OrdersViewModel(ordersData);
+            var addOrderViewModel = new AddOrderViewModel();
+            var orderDetailsViewModel = new OrderDetailsViewModel();
+            var addItemViewModel = new AddItemViewModel(ordersData.StockItems);
+            var viewModel = new MainWindowViewModel(ordersData, currentViewModel, addItemViewModel)
+            {
+                AddOrderViewModel = addOrderViewModel,
+                OrdersViewModel = currentViewModel,
+                OrderDetailsViewModel = orderDetailsViewModel
+            };
+            viewModel.SubscribeHandlersToEvents();
             Application.Current.MainWindow = new MainWindow
             {
                 DataContext = viewModel
