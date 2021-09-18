311688ee5275bdcaa27a428d09a85727fe92960d
MrGForce <willortizloza@hotmail.com>
2021-09-19 02:32:51 +1000

    Start OrderDetailsViewModel in progress

--- OrderMgmtSystem/ViewModels/OrderDetailsViewModel.cs
+++ OrderMgmtSystem/ViewModels/OrderDetailsViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace OrderMgmtSystem.ViewModels
+﻿using DataModels;
+using OrderMgmtSystem.Commands;
+using OrderMgmtSystem.Services;
+using OrderMgmtSystem.Services.Dialogs;
+using System.Collections.Generic;
+
+namespace OrderMgmtSystem.ViewModels
 {
-    class OrderDetailsViewModel : ViewModelBase
+    public class OrderDetailsViewModel : ViewModelBase
     {
+        private Order _order;
+        private readonly IDialogService _dialogservice;
+
+        public Order Order { get => _order; set => _order = value; }
+        public List<OrderItem> OrderItems { get; private set; }
+        public string Title { get; }
+        public OrderDetailsViewModel(Order order)
+        {
+            Order = order;
+            Title = $"Order number: {order.Id}";
+            _dialogservice = new DialogService();
+        }
     }
 }
