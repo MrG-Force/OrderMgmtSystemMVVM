2c3a8fe0d1589752a0829af90479e209dad2c436
MrGForce <willortizloza@hotmail.com>
2021-09-19 02:30:10 +1000

    Add child Window functionality for Order details

---
+++ OrderMgmtSystem/Services/Windows/ChildWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace OrderMgmtSystem.Services.Windows
+{
+    /// <summary>
+    /// Interaction logic for ChildWindow.xaml
+    /// </summary>
+    public partial class ChildWindow : Window
+    {
+        public ChildWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OrderMgmtSystem/Services/Windows/ChildWindowService.cs
@@ -0,0 +1,29 @@
+﻿using OrderMgmtSystem.ViewModels;
+using System;
+
+namespace OrderMgmtSystem.Services.Windows
+{
+    public class ChildWindowService
+    {
+        public void OpenWindow(ViewModelBase viewModel)
+        {
+            ChildWindow window = new ChildWindow();
+            window.DataContext = viewModel;
+            window.Show();
+            window.Closing += OnWindow_Closing;
+        }
+
+        public event Action<int> ChildWindowClosed = delegate { };
+        private void OnChildWindowClosed(int orderId)
+        {
+            ChildWindowClosed(orderId);
+        }
+
+        private void OnWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ChildWindow obj = (ChildWindow)sender;
+            OrderDetailsViewModel vm = (OrderDetailsViewModel)obj.DataContext;
+            OnChildWindowClosed(vm.Order.Id);
+        }
+    }
+}
