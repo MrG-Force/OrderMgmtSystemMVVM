e2d4d9eaf28e0dd34ed30f4b3ddb880fbabd2bfa
MrGForce <willortizloza@hotmail.com>
2021-09-19 13:23:07 +1000

    Add a bunch of docstrings to methods

--- OrderMgmtSystem/Services/Windows/ChildWindowService.cs
+++ OrderMgmtSystem/Services/Windows/ChildWindowService.cs
@@ -5,6 +5,11 @@ namespace OrderMgmtSystem.Services.Windows
 {
     public class ChildWindowService
     {
+        /// <summary>
+        /// Starts and shows a new Window with the passed viewModel and
+        /// wires up the window.Closing event.
+        /// </summary>
+        /// <param name="viewModel"></param>
         public void OpenWindow(ViewModelBase viewModel)
         {
             ChildWindow window = new ChildWindow();
@@ -14,11 +19,23 @@ namespace OrderMgmtSystem.Services.Windows
         }
 
         public event Action<int> ChildWindowClosed = delegate { };
+
+        /// <summary>
+        /// Raises the ChildWindowClosed event passing the orderId
+        /// to the subscribed listeners.
+        /// </summary>
+        /// <param name="orderId"></param>
         private void OnChildWindowClosed(int orderId)
         {
             ChildWindowClosed(orderId);
         }
 
+        /// <summary>
+        /// Handles the window Closing event by calling OnChildWindowClosed
+        /// passing the corresponding order Id from the sender.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             ChildWindow obj = (ChildWindow)sender;
