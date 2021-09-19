ed3f048f3abb166835fac9bb0b8503bfff46df0d
MrGForce <willortizloza@hotmail.com>
2021-09-19 13:23:54 +1000

    Implement ICloseWindows and corresponding command

--- OrderMgmtSystem/ViewModels/OrderDetailsViewModel.cs
+++ OrderMgmtSystem/ViewModels/OrderDetailsViewModel.cs
@@ -2,23 +2,36 @@
 using OrderMgmtSystem.Commands;
 using OrderMgmtSystem.Services;
 using OrderMgmtSystem.Services.Dialogs;
+using OrderMgmtSystem.Services.Windows;
+using System;
 using System.Collections.Generic;
 
 namespace OrderMgmtSystem.ViewModels
 {
-    public class OrderDetailsViewModel : ViewModelBase
+    public class OrderDetailsViewModel : ViewModelBase, ICloseWindows
     {
-        private Order _order;
         private readonly IDialogService _dialogservice;
 
-        public Order Order { get => _order; set => _order = value; }
+        public Order Order { get; set; }
         public List<OrderItem> OrderItems { get; private set; }
         public string Title { get; }
+        public Action Close { get; set; }
+        public DelegateCommand CloseWindowCommand { get; private set; }
+
         public OrderDetailsViewModel(Order order)
         {
             Order = order;
             Title = $"Order number: {order.Id}";
+            CloseWindowCommand = new DelegateCommand(CloseWindow);
             _dialogservice = new DialogService();
+        }
+
+        /// <summary>
+        /// Invokes the Close delegate.
+        /// </summary>
+        private void CloseWindow()
+        {
+            Close?.Invoke();
         }
     }
 }
