d6268c323a0f479005612ae70d3987ea02c33c17
MrGForce <willortizloza@hotmail.com>
2021-09-20 22:18:05 +1000

    ChildWindowService works almost like a viewmodel now

--- OrderMgmtSystem/Services/Windows/ChildWindowService.cs
+++ OrderMgmtSystem/Services/Windows/ChildWindowService.cs
@@ -1,24 +1,56 @@
-﻿using OrderMgmtSystem.ViewModels;
+﻿using DataModels;
+using OrderMgmtSystem.ViewModels;
 using System;
 
 namespace OrderMgmtSystem.Services.Windows
 {
     public class ChildWindowService
     {
+        private ChildWindow _childWindow;
+        private ViewModelBase _orderDetailsviewModel;
+        private AddItemViewModel _addItemVM;
+        private EditOrderViewModel _editOrderVM;
+
+        public event Action<int> ChildWindowClosed = delegate { };
+
+        public ChildWindowService(ViewModelBase OrderDetailsVM, ViewModelBase editOrderVM)
+        {
+            _childWindow = new ChildWindow();
+            _orderDetailsviewModel = OrderDetailsVM;
+            if (_orderDetailsviewModel is OrderDetailsViewModel odvm
+                && editOrderVM is EditOrderViewModel edovm)
+            {
+                odvm.EditOrderRequested += OnEditOrderRequested;
+                _addItemVM = odvm.AddItemViewModel;
+                _editOrderVM = edovm;
+                _editOrderVM.OrderUpdated += OnOrderUpdated;
+                
+            }
+        }
+
+        private void OnOrderUpdated()
+        {
+            _childWindow.DataContext = _orderDetailsviewModel;
+        }
+
+        private void OnEditOrderRequested(Order order)
+        {
+            EditOrderViewModel editOrderVM = new EditOrderViewModel(order, _addItemVM);
+            _childWindow.DataContext = editOrderVM;
+        }
+
         /// <summary>
         /// Starts and shows a new Window with the passed viewModel and
         /// wires up the window.Closing event.
         /// </summary>
         /// <param name="viewModel"></param>
-        public void OpenWindow(ViewModelBase viewModel)
+        public void OpenWindow()
         {
-            ChildWindow window = new ChildWindow();
-            window.DataContext = viewModel;
-            window.Show();
-            window.Closing += OnWindow_Closing;
+            _childWindow.DataContext = _orderDetailsviewModel;
+            _childWindow.Show();
+            _childWindow.Closing += OnWindow_Closing;
         }
 
-        public event Action<int> ChildWindowClosed = delegate { };
 
         /// <summary>
         /// Raises the ChildWindowClosed event passing the orderId
@@ -39,7 +71,7 @@ namespace OrderMgmtSystem.Services.Windows
         private void OnWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             ChildWindow obj = (ChildWindow)sender;
-            OrderDetailsViewModel vm = (OrderDetailsViewModel)obj.DataContext;
+            IHandleOneOrder vm = (IHandleOneOrder)obj.DataContext;
             OnChildWindowClosed(vm.Order.Id);
         }
     }
