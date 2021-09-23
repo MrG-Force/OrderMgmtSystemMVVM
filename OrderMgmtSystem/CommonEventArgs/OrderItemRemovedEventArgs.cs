ef2c8c2b2f2b1d62b0c9b199598c755f6a1df4d5
MrGForce <willortizloza@hotmail.com>
2021-09-23 19:19:37 +1000

    Create base class to AddOrderVM and OrderDetailsVM

---
+++ OrderMgmtSystem/CommonEventArgs/OrderItemRemovedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OrderMgmtSystem.CommonEventArgs
+{
+    public class OrderItemRemovedEventArgs : EventArgs
+    {
+        public int StockItemId { get; set; }
+        public int Quantity { get; set; }
+        public int OnBackOrder { get; set; }
+    }
+}
