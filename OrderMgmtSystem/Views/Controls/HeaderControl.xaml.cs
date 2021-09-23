819e6eeddcb67d82317a1ae5231f4d7d9e144257
MrGForce <willortizloza@hotmail.com>
2021-09-23 19:28:38 +1000

    Move a folder, probably not a great idea

---
+++ OrderMgmtSystem/Views/Controls/HeaderControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OrderMgmtSystem.Controls
+{
+    /// <summary>
+    /// Interaction logic for HeaderControl1.xaml
+    /// </summary>
+    public partial class HeaderControl : UserControl
+    {
+        public HeaderControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OrderMgmtSystem/Views/Controls/ModalHeaderControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OrderMgmtSystem.Controls
+{
+    /// <summary>
+    /// Interaction logic for ModalHeaderControl.xaml
+    /// </summary>
+    public partial class ModalHeaderControl : UserControl
+    {
+        public ModalHeaderControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
