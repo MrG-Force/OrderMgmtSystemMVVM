af07b9bcf4029557931a36e2bc7ee08fe0649e40
MrGForce <willortizloza@hotmail.com>
2021-09-27 20:22:21 +1000

    New user contro with its view model for reject orders.

---
+++ OrderMgmtSystem/ViewModels/DialogViewModels/RejectOrderDialogViewModel.cs
@@ -0,0 +1,30 @@
+﻿using OrderMgmtSystem.Commands;
+using OrderMgmtSystem.Services;
+using System.Windows.Input;
+
+namespace OrderMgmtSystem.ViewModels.DialogViewModels
+{
+    internal class RejectOrderDialogViewModel : DialogViewModelBase<bool>
+    {
+        public RejectOrderDialogViewModel(string title, string message, string warning) : base(title, message)
+        {
+            ProceedCommand = new DelegateCommand<IDialogWindow>(Proceed);
+            CancelCommand = new DelegateCommand<IDialogWindow>(Cancel);
+            Warning = warning;
+        }
+
+        public string Warning { get; set; }
+
+        public ICommand ProceedCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+
+        private void Proceed(IDialogWindow window)
+        {
+            CloseDialogWithResult(window, true);
+        }
+        private void Cancel(IDialogWindow window)
+        {
+            CloseDialogWithResult(window, false);
+        }
+    }
+}
