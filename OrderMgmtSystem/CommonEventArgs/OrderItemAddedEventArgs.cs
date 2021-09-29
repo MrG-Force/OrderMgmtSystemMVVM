73c9ca909e5773286a47c319900be90a583b3ee1
MrGForce <willortizloza@hotmail.com>
2021-09-29 23:44:47 +1000

    New class for new needs

---
+++ OrderMgmtSystem/CommonEventArgs/OrderItemAddedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using DataModels;
+using System;
+
+namespace OrderMgmtSystem.CommonEventArgs
+{
+    /// <summary>
+    /// Provides the relevant event data to handle adding new or existing items to an order.
+    /// </summary>
+    public class OrderItemAddedEventArgs : EventArgs
+    {
+        public OrderItem Item { get; set; }
+        public bool OrderItemExists { get; set; }
+    }
+}
