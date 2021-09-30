7c0ad66ba1b6eec346d1565b939ac466683b98e6
MrGForce <willortizloza@hotmail.com>
2021-09-30 23:19:41 +1000

    Put this eventArgs back with one more property

---
+++ OrderMgmtSystem/CommonEventArgs/OrderItemRemovedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderMgmtSystem.CommonEventArgs
+{
+    /// <summary>
+    /// This class provides the relevant even information to handle an ItemRemovedEvent
+    /// </summary>
+    public class OrderItemRemovedEventArgs : EventArgs
+    {
+        public int OrderHeaderId { get; set; }
+        public int StockItemId { get; set; }
+        public int Quantity { get; set; }
+        public int OnBackOrder { get; set; }
+    }
+}
