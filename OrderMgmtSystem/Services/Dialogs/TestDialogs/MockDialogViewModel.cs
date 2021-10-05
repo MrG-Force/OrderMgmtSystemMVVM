a5bc42f956ee8ae84cda8a51d3295335bba650b5
MrGForce <willortizloza@hotmail.com>
2021-10-05 22:42:52 +1100

    Inherit from QuantityViewModel so the mock object can be properly cast

--- OrderMgmtSystem/Services/Dialogs/TestDialogs/MockDialogViewModel.cs
+++ OrderMgmtSystem/Services/Dialogs/TestDialogs/MockDialogViewModel.cs
@@ -1,8 +1,10 @@
-﻿namespace OrderMgmtSystem.Services.Dialogs.TestDialogs
+﻿using OrderMgmtSystem.ViewModels.DialogViewModels;
+
+namespace OrderMgmtSystem.Services.Dialogs.TestDialogs
 {
-    public class MockDialogViewModel : DialogViewModelBase<int>
+    public class MockDialogViewModel : QuantityViewModel
     {
-        public MockDialogViewModel()
+        public MockDialogViewModel(string title, string message): base(title, message)
         {
             DialogResult = 3;
         }
